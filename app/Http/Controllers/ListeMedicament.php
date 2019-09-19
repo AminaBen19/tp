04a854be5a433dd126570d422eb611ed46a0967c
as mae <asma18ben@gmail.com>
2019-09-19 19:57:53 +0100

    Deleted

--- app/Http/Controllers/ListeMedicament.php
+++ app/Http/Controllers/ListeMedicament.php
@@ -55,20 +55,18 @@ $forme = DB::table('forme')
 
 
 
-public function search(Request $request){
+public function search(){
 $search = $request->get('search'); 
 
 $med = DB::table('medicament')->where('nom','like','%'.$search.'%')
                               ->orWhere('dosage','like','%'.$search.'%')
-                              ->paginate(10);
-if($request->forme){
-                                $med=DB::table('medicament')
-                                       ->select('medicament.idM','medicament.nom','forme.forme_name'
-                                       ,'medicament.famille')
-                                       ->where('medicament.forme',$request->forme);                                }
-                             
+                              ->orWhere('forme','like','%'.$search.'%')
+                              ->orWhere('famille','like','%'.$search.'%')
+                              ->paginate(20);
 return view('medicamentPre',['med' => $med]);
-}
+                            
+
+                            }
 
 
 }
